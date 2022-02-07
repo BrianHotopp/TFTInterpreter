4895cbe565b2a98bde3ae6aac3320a3bc1251649
BrianHotopp <brihoto@gmail.com>
2022-02-07 17:54:12 -0500

    heuristics instead of number for planning phase detection

--- gather_data/screenshotter.py
+++ gather_data/screenshotter.py
@@ -18,38 +18,20 @@ def get_top_numbers():
             height_of_numbers
         ))
     return im2
-
 if __name__ == "__main__":
     # full screen
     #im = pyautogui.screenshot("test.png", region=(0,offset, 1920, 1080))
+    helmet_confidence = 0.6 
     while True:
-        # check if the top numbers changed
-        lastPIL = get_top_numbers()
-        time.sleep(1)
-        newPIL = get_top_numbers()
-        open_cv_image1 = numpy.array(lastPIL)
-        open_cv_image1 = open_cv_image1[:, :, ::-1].copy()  
-        open_cv_image2 = numpy.array(newPIL) 
-        open_cv_image2 = open_cv_image2[:, :, ::-1].copy() 
-
-        # do with image1
-        hist1 = cv2.calcHist([open_cv_image1], [0], None, [256], [0, 256])
-        hist1 = cv2.normalize(hist1, hist1).flatten()
-        # do with image2
-        hist2 = cv2.calcHist([open_cv_image2], [0], None, [256], [0, 256])
-        hist2 = cv2.normalize(hist2, hist2).flatten()
-        # if compare is > 0 then top numbers changed
-        compare = cv2.compareHist(hist1, hist2, 1) 
-        # check to make sure we are not in a carousel round
-        im = pyautogui.locateOnScreen('resources/noncarousel.PNG', confidence = 0.8)
-        im2 = pyautogui.locateOnScreen('resources/noncarousel2.PNG', confidence = 0.8)
-        planning_phase = compare > 0 and (im is not None or im2 is not None)
-        if planning_phase:
+        in_carousel = pyautogui.locateOnScreen('resources/carousel.PNG', confidence = 0.9, region=(0,TOP_BAR_THICKNESS, RES[0], RES[1])) != None
+        gray_helmet = pyautogui.locateOnScreen('resources/helmet.PNG', confidence = helmet_confidence, region=(0,TOP_BAR_THICKNESS, RES[0], RES[1])) != None
+        blue_helmet = pyautogui.locateOnScreen('resources/helmetblue.PNG', confidence = helmet_confidence, region=(0,TOP_BAR_THICKNESS, RES[0], RES[1])) != None
+        augment_button = pyautogui.locateOnScreen('resources/augmentbutton.PNG', confidence = 0.9, region=(0,TOP_BAR_THICKNESS, RES[0], RES[1])) != None
+        enemy_bench_empty = pyautogui.locateOnScreen('resources/emptybench.PNG', confidence = 0.8, region=(0,TOP_BAR_THICKNESS, RES[0], RES[1])) != None
+        planning = blue_helmet or gray_helmet and not in_carousel and not augment_button and enemy_bench_empty
+        print(f"bluehelm {blue_helmet} grayhelm {gray_helmet} incarousel {in_carousel} augment_button {augment_button} enemybenchempty {enemy_bench_empty}")
+        if planning:
             print("in planning phase")
-            for i in range(5):
-                print("taking screenshot")
-                im = pyautogui.screenshot(RAW_SCREENSHOT_DIR+str(uuid.uuid4())+".png", region=(0,TOP_BAR_THICKNESS, RES[0], RES[1]))
-                time.sleep(1)
-        else:
-            print("not in planning phase")
-        
+            im = pyautogui.screenshot(RAW_SCREENSHOT_DIR+str(uuid.uuid4())+".png", region=(0,TOP_BAR_THICKNESS, RES[0], RES[1]))
+            time.sleep(1)
+
