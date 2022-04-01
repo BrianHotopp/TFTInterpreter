6590525da7e55c8d50da4e435b8db9963ae956d1
DanaSolitaire <solitaire.dana@gmail.com>
2022-04-01 17:44:55 -0400

    Done
    returns list of lowercase unit names

--- src/app/SynergyClass.py
+++ src/app/SynergyClass.py
@@ -1,17 +1,14 @@
-import pandas as pd
-import networkx as nx
-import numpy as np
 
 '''
 Class Snode
 
 SNode is a mutable node which contains the list of champs associated 
-with that particular origin or class. 
-name = name of class or origin
+with that particular composition
+name = name of composition
 champs = list of champions
 
 @effects prints a string representation of the SNode
-printNode()
+printNode(self)
 
 No collaborators
 '''
@@ -19,7 +16,7 @@ class SNode:
 
    def __init__(self, name, champ):
       self.champs = [champ]
-      self.name = name #name of class or origin
+      self.name = name
       
    def printNode(self):
       print("NAME: ", self.name, "CHAMPS: ", self.champs)
@@ -27,69 +24,143 @@ class SNode:
 '''
 Class SGraph
 
-This is a mutable graph which contains a dictionary of Synergy nodes. Each node contains all the champions 
-connected to that origin or class. 
+This is a mutable graph which contains a dictionary of SNode nodes. Each node contains all the champions 
+connected to a specific composition
+
 SDict = Dictionary of SNodes
-possibleSyn = list of possible synergies. this list is returned in the findSyn()
-file = name of file containing all of the data in edge list format 
+possibleUnits = list of possible units that would fill a composition
+possibleComps = list of possible compositions
+file = name of file containing all of the data 
 line = current line we are reading from file
-name = name of origin or class
+name = name of composition
 champ = champoin associated with the name 
-tempList = temporary list used to store current list of champions associated with a specific origin/ class
+tempList = temporary list used to store current list of champions associated with a specific composition
 champList = list of current champions on the board. this list is used in findSyn()
 
 @param champList 
-@effects creates list of possible synergies
-@return possible list of synergies
-findSyn(given champion list)
+@effects creates list of compositions
+@return possible list of units 
+findComp(given champion list)
 
 @effects reads in file and creates an SGraph
 @modifies creates and fills the SDict with SNodes
-readfile()
+readfile(self, given file name)
 
-I collaborated with Brian Hotopp while creating findSyn() 
+I collaborated with Brian Hotopp while creating findComp(given champ list) 
 '''
 class SGraph:
    def __init__(self):
-      self.SDict = {}
-   #if champ is in specific synergy than add that synNode to possible 
-   #list of class/origins   
-   def findSyn(champList):
-      possibleSyn = []
-      for champ in champList:
-         for synNode in SDict:
-            if (SDict[synNode].champs.count(champ) == 1 and possibleSyn.count(synNode) == 0):
-               possibleSyn.append(SDict[synNode].name)  
-               
-   def readFile():
-      file = open("champEdge.txt", "rb")    
-      with open("champEdge.txt") as file:
-         for line in file:
-            line = line.split(":")
-            #print(line[1])
-            #setting up data  
-            name = line[0] 
-            champ = line[1]
-            # SDict = "Name" : Synergy Node
-            #Checks if class or origin exists in SDict if not add, if it is there update SNode.champs and add champ      
-            if (name in SDict and SDict[name].champs.count(champ) == 0):
-               tempList = SDict[name].champs
-               tempList.append(champ)
-               SDict[name].champs = tempList         
-            else:
-               SDict[name] = SNode(name,champ) 
-         file.close()
-         
-#"champEdge.txt"      
-'''
-champList = ['Ahri 4\n', 'Ashe 2\n', 'Braum 4\n', 'Darius 1\n', 'Morgana 3\n', 'Zyra 2\n']
-print(SDict.keys())
-possibleSyn = []
-for champ in champList:
-   for synNode in SDict:
-      if (SDict[synNode].champs.count(champ) == 1 and possibleSyn.count(synNode) == 0):
-         possibleSyn.append(SDict[synNode].name)    
-print(possibleSyn)
+      self.lvl4 = {
+         0: ["poppy","ziggs","blitzcrank","vex"],
+         1:["darius","morgana","senna","braum"],
+         2:["ziggs","corki","vex","jinx"],
+         3:["ziggs","gnar","vex","irelia"],
+         4:["darius","zyra","braum","silco"],
+         5:["jarvaniv","lucian","irelia","jinx"],
+         6:["warwick","tryndamere","renata","silco"],
+         7:["lucian","irelia","sivir","jinx"]
+         }
+      self.lvl5= { 0: ["caitlyn","zilean","jhin","seraphine","jayce"] }
+      self.lvl6 ={}
+      self.lvl7 = {}
+      
+   #find what comps have champs in there
+   #if the comps already in possibleComps list skip
+   #if not add that comp to possibleComps
+   #Then match the comps from possibleComps 
+   #return what units aren't in comps in possibleUnits
+   
+   
+   def findComp(self,champList):
+      for i in range (len(champList)):
+         champList[i] = champList[i].lower()
+      champList = set(champList)
+      possibleUnits = {}
+      possibleComps = {}
+      
+      if(len(champList) <= 4):
+         for i in range(8):
+            #lvl 4
+            diffSet= set(self.lvl4.get(i)).difference(champList)
+            if (len(diffSet) != len(self.lvl4.get(i)) and len(diffSet) != 0):
+               index = i + 400
+               possibleComps[index] = diffSet
+      
+         #lvl 5
+      diffSet= set(self.lvl5.get(0)).difference(champList)
+      if (len(diffSet) != len(self.lvl5.get(0))):
+         possibleComps[500] = diffSet
+            
+      if(len(champList) >=5):
+         for i in range(71):
+            diffSet= set(self.lvl6.get(i)).difference(champList)
+            if (len(diffSet) != len(self.lvl6.get(i)) and len(diffSet) != 5):
+               index = i + 600
+               possibleComps[index] = diffSet   
+              
+         for i in range(25):
+            diffSet= set(self.lvl7.get(i)).difference(champList)
+            if (len(diffSet) != len(self.lvl7.get(i))):
+               index = i + 700
+               possibleComps[index] = diffSet    
+      
+      unitDict = possibleComps.popitem()
+      unitSet = unitDict[1]
+      for i in range (len(possibleComps)):
+         unitDict = possibleComps.popitem()
+         unitSet = unitSet.union(unitDict[1])
+         possibleUnits["Consider buying these units: "] = unitSet
 
-#print(SDict["Syndicate"].champs)
-''' 
+      return list(unitSet)
+   
+   def readFile(self):
+      compIndex = -1
+      file = open("lvl7.txt","rb")
+      with open("lvl7.txt") as file:
+         for line in file:
+            line = line.split("[")
+            line = line[1].split(",")
+            compIndex += 1
+            tempList = []
+            for i in range (len(line)-1):
+               name = line[i].strip('""')
+               tempList.append(name)    
+            name = line[-1].strip('""')  
+            if(name[-1] == ']'):
+               name = name[0:-3]
+            else:
+               name = name[0:-5]
+            tempList.append(name)
+            self.lvl7[compIndex] = tempList
+      file.close()
+      
+      compIndex = -1
+      file = open("lvl6.txt","rb")
+      with open("lvl6.txt") as file:
+         for line in file:
+            line = line.split("[")
+            line = line[1].split(",")
+            compIndex += 1
+            tempList = []
+            for i in range (len(line)-1):
+               name = line[i].strip('""')
+               tempList.append(name)    
+            name = line[-1].strip('""')  
+            if(name[-1] == ']'):
+               name = name[0:-3]
+            else:
+               name = name[0:-5]
+            tempList.append(name)
+            self.lvl6[compIndex] = tempList
+      file.close()
+'''
+sample main:
+G = SGraph() 
+G.readFile()
+G.findComp(["Lucian","irElia","sivir","jinx",'swain',"zilean"])
+output: ['corki', 'galio', 'gnar', 'illaoi', 'viktor', 'singed', 'camille', 
+         'zac', 'vex', 'orianna', 'alistar', 'poppy', 'seraphine', 'silco', 
+         'ziggs', 'lulu', 'warwick', 'jarvaniv', 'vi', 'tryndamere', 'renata', 
+         'tahmkench', 'blitzcrank', 'ezreal', 'jayce', 'jhin', 'caitlyn', 
+         'sejuani', 'senna', 'gangplank']
+'''
