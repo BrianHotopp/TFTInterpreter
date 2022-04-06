1a7480a0ef70579c4afdc3fac55cdbac003a6e87
Brian Hotopp <brihoto@gmail.com>
2022-04-06 11:20:47 -0400

    fixed paths to support running tests from project root, changed classmethod to static method, passing tests for check_in_planning_phase

--- test/app/test_continuous_inference.py
+++ test/app/test_continuous_inference.py
@@ -5,19 +5,16 @@ from pathlib import Path
 
 class TestClass:
     def test_in_planning_phase(self):
-        im0p = Path("/Users/sarvesh/Documents/TFTInterpreter/test/app/test_resources/0.png")
+        im0p = Path("test/app/test_resources/0.png")
         with Image.open(im0p) as im0:
             assert(Predictor.in_planning_phase(im0))
-
-        im1p = Path("/Users/sarvesh/Documents/TFTInterpreter/test/app/test_resources/1.png")
+        im1p = Path("test/app/test_resources/1.png")
         with Image.open(im1p) as im1:
             assert(Predictor.in_planning_phase(im1))
-
-        im2p = Path("/Users/sarvesh/Documents/TFTInterpreter/test/app/test_resources/2.png")
+        im2p = Path("test/app/test_resources/2.png")
         with Image.open(im2p) as im2:
             assert(Predictor.in_planning_phase(im2))
-
-        im3p = Path("/Users/sarvesh/Documents/TFTInterpreter/test/app/test_resources/3.png")
+        im3p = Path("test/app/test_resources/3.png")
         with Image.open(im3p) as im3:
             assert(Predictor.in_planning_phase(im3))
 
