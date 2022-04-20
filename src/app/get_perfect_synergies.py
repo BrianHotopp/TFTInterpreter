6b7867f89ca1e4e4337052251e41851acbe79601
Brian Hotopp <brihoto@gmail.com>
2022-04-20 01:18:21 -0400

    WIP

--- src/app/get_perfect_synergies.py
+++ src/app/get_perfect_synergies.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 import itertools
+from re import M
 import numpy as np
+import queue 
+import random
+def load_breakpoints(path):
+    """
+    load breakpoints from a file
+    """
 def load_units(path):
     """
     Loads units from a file.
@@ -11,7 +18,7 @@ def load_units(path):
     # each line has format name, cost, origin1, origin2, class1, class2 
     # parse each line
     units = [unit.split(",") for unit in units]
-    # remove newline characters
+    # remove newline characters and whitespace
     units = [[unit[0].strip(), int(unit[1].strip()), unit[2].strip(), unit[3].strip(), unit[4].strip(), unit[5].strip()] for unit in units]
     # get a set of all unit names
     unit_names = set([unit[0] for unit in units])
@@ -20,17 +27,17 @@ def load_units(path):
     # get a set of all classes
     classes = set([unit[4] for unit in units]).union(set([unit[5] for unit in units]))
     # create a dictionary mapping number to unit name
-    unit_dict = {i:unit[0] for i, unit in enumerate(unit_names)}
+    unit_dict = {i:unit for i, unit in enumerate(unit_names)}
     # inverse of unit_dict
-    unit_dict_inv = {unit[0]:i for i, unit in enumerate(unit_names)}
+    unit_dict_inv = {unit:i for i, unit in enumerate(unit_names)}
     # create a dict mapping number to origin
-    origin_dict = {i:unit[2] for i, unit in enumerate(origins)}
+    origin_dict = {i:origin for i, origin in enumerate(origins)}
     # inverse of origin_dict
-    origin_dict_inv = {unit[2]:i for i, unit in enumerate(origins)}
+    origin_dict_inv = {origin:i for i, origin in enumerate(origins)}
     # create a dict mapping number to class
-    class_dict = {i:unit[4] for i, unit in enumerate(classes)}
+    class_dict = {i:cls for i, cls in enumerate(classes)}
     # inverse of class_dict
-    class_dict_inv = {unit[4]:i for i, unit in enumerate(classes)}
+    class_dict_inv = {cls:i for i, cls in enumerate(classes)}
     # convert the units list to numbers using the dictionaries  
     units = [[unit_dict_inv[unit[0]], unit[1], origin_dict_inv[unit[2]], origin_dict_inv[unit[3]], class_dict_inv[unit[4]], class_dict_inv[unit[5]]] for unit in units]
     # to np array for big zoom zoom
@@ -42,37 +49,70 @@ def get_prefixes(units, prefix_size):
     returns an iterator of all possible 2 unit prefixes (uses indexes)
     """
     return itertools.combinations(range(units.shape[0]), prefix_size)
+def get_ranges(l, p):
+    """
+    """
+    # the prefix is in sorted order already
+    ranges = []
+    for i in range(len(p)):
+        if i == 0:
+            ranges.append(range(p[i]))
+        else:
+            ranges.append(range(p[i-1]+1, p[i]))
+    ranges.append(range(min(p[-1]+1, l), l))
+    return ranges
+
 def get_unused_it(units, prefix):
     """
     prefix: list of indexes we should not use
     returns an iterator of all possible combinations of units excluding the prefix (uses indexes)
     """
-    # the prefix is in sorted order already
-    ranges = []
-    for i in range(len(prefix)-1):
-        if i == 0:
-            ranges.append(range(prefix[i]))
-        else:
-            ranges.append(range(prefix[i-1]+1, prefix[i]))
-    # the last range is the last index
-    ranges.append(range(prefix[-1], units.shape[0]))
+    """
+    units has length n
+    [|0|1|] -> [2, 3, 4, 5,... n]
+    [1, 2] -> [0, 3, 4, 5, ... n]
+    """
+    ranges = get_ranges(units.shape[0], prefix)
     return itertools.chain(*ranges)
-    
-def get_combinations(units, prefix):
+
+def get_partial_combination(units, prefix, size):
     """
     units: np array
     prefix: list of indexes we should not use
     returns an iterator of all possible combinations of units excluding the prefix (uses indexes)
+    each element is a list of indexes of length size-len(prefix)
     """
-    # the prefix is in sorted order already
-    ranges = []
-    for i in range(len(prefix)):
-        if i == 0:
-            ranges.append(range(prefix[i]))
+    unused = get_unused_it(units, prefix)
+    return itertools.combinations(unused, size-len(prefix))
+def dummy_measure(units, full):
+    return sum(full)
+def best_by_measure(units, prefix, size, measure, priority_queue):
+    """
+    units: np array
+    prefix: list of indexes we should not use
+    size: number of units to choose
+    measure: function to use to evaluate a combination
+    priority_queue: thread safe priority queue
+    """
+    for postfix in get_partial_combination(units, prefix, size):
+        full = prefix + postfix
+        measure_value = measure(units, full)
+        # attempt to add to the priority queue if measure value is better (larger) than the worst element
+        if priority_queue.full():
+            if measure_value > priority_queue.queue[0][0]:
+                # remove the worst element
+                priority_queue.get()
+                # add the new element
+                priority_queue.put((measure_value, full))
         else:
-            ranges.append(range(prefix[i-1]+1, prefix[i]))
-    itertools.chain(*ranges)
+            priority_queue.put((measure_value, full))
 
+def best_overall(units, prefix_size, size, measure, top_n):
+    best = queue.PriorityQueue(maxsize=top_n)
+    for prefix in get_prefixes(units, prefix_size):
+        best_by_measure(units, prefix, size, measure, best)
+    return best
+ 
 def main():
     units_data_path = Path("src/resources/champs.csv")
     units_data = load_units("units.txt")
--- test/app/test_get_perfect_synergies.py
+++ test/app/test_get_perfect_synergies.py
@@ -1,17 +1,47 @@
 from pathlib import Path
 import itertools
 import numpy as np
-from src.app.get_perfect_synergies import load_units, get_prefixes, get_combinations, get_unused_it
+import src.app.get_perfect_synergies as gps
 
 class TestClass:
-    def test_get_unused_it(self):
+    def test_load_units(self):
         path = Path("test/app/test_resources/champs.csv")
-        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = load_units(path)
+        # load the units
+        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = gps.load_units(path)
+        # check that the units are the correct length
+        assert units.shape[0] == len(unit_dict)
+        # check that the unit_dict is the same length as the inverse
+        assert len(unit_dict) == len(unit_dict_inv)
+        # check that the origin dict is the same length as the inverse
+        assert len(origin_dict) == len(origin_dict_inv)
+        # check that the class dict is the same length as the inverse
+        assert len(class_dict) == len(class_dict_inv)
+    def test_get_ranges(self):
+        path = Path("test/app/test_resources/champs.csv")
+        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = gps.load_units(path)
         prefix_size = 2
         prefix = [0, 1]
-        unused_it = get_unused_it(units, prefix)
+        ranges = gps.get_ranges(units.shape[0], prefix)
+        # print the endpoints of each range
+        for r in ranges:
+            print(r.start, r.stop)
+        # check that the ranges are the correct length
+        assert len(ranges) == prefix_size + 1
+        # check that the ranges don't overlap
+        for i in range(len(ranges) - 1):
+            assert ranges[i].stop < ranges[i+1].start
+        # check that the ranges don't go past the end of the array
+        assert ranges[-1].stop == units.shape[0]
+
+    def test_get_unused_it(self):
+        path = Path("test/app/test_resources/champs.csv")
+        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = gps.load_units(path)
+        prefix_size = 2
+        prefix = [0, 1]
+        unused_it = gps.get_unused_it(units, prefix)
         unused_it_list = list(unused_it)
         # check that the unused_it is the correct length
+        print(unused_it_list)
         assert len(unused_it_list) == units.shape[0] - prefix_size
         # check that unused it does not contain the prefix
         for i in prefix:
@@ -20,4 +50,34 @@ class TestClass:
         for i in range(units.shape[0]):
             if i not in prefix:
                 assert i in unused_it_list
-
+    def test_get_partial_combination(self):
+        path = Path("test/app/test_resources/champs.csv")
+        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = gps.load_units(path)
+        prefix_size = 2
+        prefix = [0, 1]
+        size = 5
+        partial_combination = gps.get_partial_combination(units, prefix, size)
+        # check that each element in the partial combination has the correct length
+        for c in partial_combination:
+            assert len(c) == size-prefix_size
+        # check that each element in the partial combination does not contain any of the prefix
+        for c in partial_combination:
+            for i in prefix:
+                assert i not in c
+    def test_best_overall(self):
+        path = Path("test/app/test_resources/champs.csv")
+        units, unit_dict, unit_dict_inv, origin_dict, origin_dict_inv, class_dict, class_dict_inv = gps.load_units(path)
+        prefix_size = 2
+        team_size = 3
+        top_n = 10
+        ovr = gps.best_overall(units, prefix_size, team_size, gps.measure, top_n)
+        # dump the queue to a list
+        ovr_list = list(ovr.queue)
+        assert len(ovr_list) == top_n
+        for i in range(top_n):
+            assert len(ovr_list[i][1]) == team_size
+        # print the scores of the top n
+        for i in range(len(ovr_list)):
+            print("Team: ", i, "Score: ", ovr_list[i])
+            print(ovr_list[i][0])
+        assert(False)
